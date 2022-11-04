1e031abf78dd9ad8e5d019bb1bd13b05e6cc733a
nizar-maatoug <nizar.maatoug12@gmail.com>
2022-11-04 15:25:08 +0100

    _mapFailureToMessage...

--- lib/features/posts/presentation/bloc/posts/posts_bloc.dart
+++ lib/features/posts/presentation/bloc/posts/posts_bloc.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:posts_app/core/exceptions/failures.dart';
+import 'package:posts_app/core/strings/failures.dart';
 import 'package:posts_app/features/posts/domain/entities/post.dart';
 import 'package:posts_app/features/posts/domain/usecases/get_all_posts.dart';
 
@@ -17,8 +19,23 @@ class PostsBloc extends Bloc<PostsEvent, PostsState> {
         final futurePosts =
             await getAllPosts(); //callable class getAllPosts().call()
 
-        futurePosts.fold((failure) {}, (posts) {});
+        futurePosts.fold((failure) {
+          emit(ErrorPostsState(message: _mapFailureToMessage(failure)));
+        }, (posts) {});
       } else if (event is RefreshPostsEvent) {}
     });
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      case EmptyCacheFailure:
+        return EMPTY_CACHE_FAILURE_MESSAGE;
+      case OfflineFailure:
+        return OFFLINE_FAILURE_MESSAGE;
+      default:
+        return "Erreur inconnue. Veuillez réessayer plus tard";
+    }
   }
 }
