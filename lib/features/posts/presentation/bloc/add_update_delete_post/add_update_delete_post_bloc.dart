4723fb6c18a9bcb0731f54c234586a1fb5e8ec21
nizar-maatoug <nizar.maatoug12@gmail.com>
2022-11-04 20:40:37 +0100

    AddPostState

--- lib/features/posts/presentation/bloc/add_update_delete_post/add_update_delete_post_bloc.dart
+++ lib/features/posts/presentation/bloc/add_update_delete_post/add_update_delete_post_bloc.dart
@@ -1,15 +1,52 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:posts_app/core/strings/messages.dart';
 import 'package:posts_app/features/posts/domain/entities/post.dart';
+import 'package:posts_app/features/posts/domain/usecases/add_post.dart';
+import 'package:posts_app/features/posts/domain/usecases/delete_post.dart';
+import 'package:posts_app/features/posts/domain/usecases/update_post.dart';
+
+import '../../../../../core/exceptions/failures.dart';
+import '../../../../../core/strings/failures.dart';
 
 part 'add_update_delete_post_event.dart';
 part 'add_update_delete_post_state.dart';
 
 class AddUpdateDeletePostBloc
     extends Bloc<AddUpdateDeletePostEvent, AddUpdateDeletePostState> {
-  AddUpdateDeletePostBloc() : super(AddUpdateDeletePostInitial()) {
+  final AddPostUsecase addPost;
+  final UpdatePostUsecase updatePost;
+  final DeletePostUsecase deletePost;
+
+  AddUpdateDeletePostBloc(
+      {required this.addPost,
+      required this.updatePost,
+      required this.deletePost})
+      : super(AddUpdateDeletePostInitial()) {
     on<AddUpdateDeletePostEvent>((event, emit) async {
-      // TODO: implement event handler
+      if (event is AddPostEvent) {
+        emit(LoadingAddUpdateDeletePostState());
+
+        final failureOrDoneMessage = await addPost(event.post);
+        failureOrDoneMessage.fold((failure) {
+          emit(ErrorAddUpdateDeletePostState(
+              message: _mapFailureToMessage(failure)));
+        }, (_) {
+          emit(MessageAddUpdateDeletePostState(message: ADD_SUCCESS_MESSAGE));
+        });
+      } else if (event is UpdatePostEvent) {
+      } else if (event is DeletePostEvent) {}
     });
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      case OfflineFailure:
+        return OFFLINE_FAILURE_MESSAGE;
+      default:
+        return "Erreur inconnue. Veuillez réessayer plus tard";
+    }
   }
 }
