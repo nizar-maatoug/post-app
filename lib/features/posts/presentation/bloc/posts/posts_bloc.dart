28a427e604d89f2bab9aa45d5ebed2ae9e09c928
nizar-maatoug <nizar.maatoug12@gmail.com>
2022-11-04 11:44:31 +0100

    posts_bloc step1

--- lib/features/posts/presentation/bloc/posts/posts_bloc.dart
+++ lib/features/posts/presentation/bloc/posts/posts_bloc.dart
@@ -1,12 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:posts_app/features/posts/domain/entities/post.dart';
+import 'package:posts_app/features/posts/domain/usecases/get_all_posts.dart';
 
 part 'posts_event.dart';
 part 'posts_state.dart';
 
 class PostsBloc extends Bloc<PostsEvent, PostsState> {
-  PostsBloc() : super(PostsInitial()) {
-    on<PostsEvent>((event, emit) {});
+  final GetAllPostsUsecase getAllPosts;
+
+  PostsBloc({required this.getAllPosts}) : super(PostsInitial()) {
+    on<PostsEvent>((event, emit) async {
+      if (event is GetAllPostsEvent) {
+        emit(LoadingPostsState());
+
+        final futurePosts =
+            await getAllPosts(); //callable class getAllPosts().call()
+
+        futurePosts.fold((failure) {}, (posts) {});
+      } else if (event is RefreshPostsEvent) {}
+    });
   }
 }
