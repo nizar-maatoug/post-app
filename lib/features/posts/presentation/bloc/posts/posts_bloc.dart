6c444ddaa16634d90fa47285ab98e0bbadf961c7
nizar-maatoug <nizar.maatoug12@gmail.com>
2022-11-04 11:24:44 +0100

    ajout bloc posts: events and states

---
+++ lib/features/posts/presentation/bloc/posts/posts_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:posts_app/features/posts/domain/entities/post.dart';
+
+part 'posts_event.dart';
+part 'posts_state.dart';
+
+class PostsBloc extends Bloc<PostsEvent, PostsState> {
+  PostsBloc() : super(PostsInitial()) {
+    on<PostsEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
