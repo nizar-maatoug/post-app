4bded617d0230ac82a674d06c4b09d265527ab04
nizar-maatoug <nizar.maatoug12@gmail.com>
2022-11-05 07:24:50 +0100

    posts page v2

--- lib/features/posts/presentation/pages/posts_page.dart
+++ lib/features/posts/presentation/pages/posts_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:posts_app/features/posts/presentation/bloc/posts/posts_bloc.dart';
 
 class PostPage extends StatelessWidget {
   const PostPage({super.key});
@@ -6,10 +8,27 @@ class PostPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Posts')),
-      body: Center(
-        child: const Text('Bonjour de posts'),
-      ),
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
+
+  AppBar _buildAppBar() => AppBar(title: const Text('Posts'));
+
+  Widget _buildBody() {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: BlocBuilder<PostsBloc, PostsState>(builder: (context, state) {
+        if (state is LoadingPostsState) {
+          return LoadingWidget();
+        } else if (state is LoadedPostsState) {
+          return PostListWidget(posts: state.posts);
+        } else if (state is ErrorPostsState) {
+          return MessageDisplayWidget(message: state.message);
+        } else {
+          return LoadingWidget();
+        }
+      }),
     );
   }
 }
