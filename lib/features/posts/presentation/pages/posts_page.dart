14591e73cc5c863dba53115bf3e2c2f6fcf576e4
nizar-maatoug <nizar.maatoug12@gmail.com>
2022-11-05 08:30:23 +0100

    posts page v3

--- lib/features/posts/presentation/pages/posts_page.dart
+++ lib/features/posts/presentation/pages/posts_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:posts_app/core/widgets/loading_widget.dart';
 import 'package:posts_app/features/posts/presentation/bloc/posts/posts_bloc.dart';
+import 'package:posts_app/features/posts/presentation/widgets/posts_widgets/message_display_widget.dart';
+import 'package:posts_app/features/posts/presentation/widgets/posts_widgets/posts_list_widget.dart';
 
 class PostPage extends StatelessWidget {
   const PostPage({super.key});
@@ -20,13 +23,13 @@ class PostPage extends StatelessWidget {
       padding: const EdgeInsets.all(10),
       child: BlocBuilder<PostsBloc, PostsState>(builder: (context, state) {
         if (state is LoadingPostsState) {
-          return LoadingWidget();
+          return const LoadingWidget();
         } else if (state is LoadedPostsState) {
           return PostListWidget(posts: state.posts);
         } else if (state is ErrorPostsState) {
           return MessageDisplayWidget(message: state.message);
         } else {
-          return LoadingWidget();
+          return const LoadingWidget();
         }
       }),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (_) => di.sl<PostsBloc>()),
+        BlocProvider(create: (_) => di.sl<PostsBloc>()..add(GetAllPostsEvent())),
         BlocProvider(create: (_) => di.sl<AddUpdateDeletePostBloc>())
       ],
       child: MaterialApp(
