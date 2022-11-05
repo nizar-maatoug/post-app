7853c1327745fd88cca558eb0350ea647b144fd4
nizar-maatoug <nizar.maatoug12@gmail.com>
2022-11-05 09:42:35 +0100

    ajouter post v2

--- lib/features/posts/presentation/pages/post_add_update_page.dart
+++ lib/features/posts/presentation/pages/post_add_update_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:posts_app/core/util/snack_bar_message.dart';
+import 'package:posts_app/core/widgets/loading_widget.dart';
+import 'package:posts_app/features/posts/presentation/bloc/add_update_delete_post/add_update_delete_post_bloc.dart';
+import 'package:posts_app/features/posts/presentation/pages/posts_page.dart';
+import 'package:posts_app/features/posts/presentation/widgets/post_add_update_widgets/form_widget.dart';
 import '../../domain/entities/post.dart';
 
 class PostAddUpdatePage extends StatelessWidget {
@@ -20,6 +26,32 @@ class PostAddUpdatePage extends StatelessWidget {
   }
 
   Widget _buildBody() {
-    return Center(child: Text('Ajouter post'));
+    return Center(
+      child: Padding(
+          padding: EdgeInsets.all(10),
+          child:
+              BlocConsumer<AddUpdateDeletePostBloc, AddUpdateDeletePostState>(
+            listener: (context, state) {
+              if (state is MessageAddUpdateDeletePostState) {
+                SnackBarMessage().showSuccessSnackBar(
+                    message: state.message, context: context);
+                Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(builder: (_) => PostPage()),
+                    (route) => false);
+              } else if (state is ErrorAddUpdateDeletePostState) {
+                SnackBarMessage().showErrorSnackBar(
+                    message: state.message, context: context);
+              }
+            },
+            builder: (context, state) {
+              if (state is LoadingAddUpdateDeletePostState) {
+                return LoadingWidget();
+              }
+
+              return FormWidget(
+                  isUpdatePost: isUpdatePost, post: isUpdatePost ? post : null);
+            },
+          )),
+    );
   }
 }
